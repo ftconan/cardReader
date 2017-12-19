3c22a018ee55d29aca33e807ef70a9558fd5f1de
pinmingkenan <1508927793@qq.com>
2017-12-19 12:47:40 +0800

    串口读写器界面完成

---
+++ cardReader/cardReader/frmMain.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace cardReader
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbbParity = new System.Windows.Forms.ComboBox();
+            this.cbbStopBits = new System.Windows.Forms.ComboBox();
+            this.cbbDataBits = new System.Windows.Forms.ComboBox();
+            this.cbbBaudRate = new System.Windows.Forms.ComboBox();
+            this.cbbComList = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.COUNT = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.btnOpen);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 572);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("SimSun", 10F);
+            this.btnOpen.Location = new System.Drawing.Point(92, 243);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(119, 40);
+            this.btnOpen.TabIndex = 11;
+            this.btnOpen.Text = "打开串口";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::cardReader.Properties.Resources.red;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 248);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbbParity);
+            this.groupBox1.Controls.Add(this.cbbStopBits);
+            this.groupBox1.Controls.Add(this.cbbDataBits);
+            this.groupBox1.Controls.Add(this.cbbBaudRate);
+            this.groupBox1.Controls.Add(this.cbbComList);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("SimSun", 10F);
+            this.groupBox1.Location = new System.Drawing.Point(0, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(237, 212);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "端口设置";
+            // 
+            // cbbParity
+            // 
+            this.cbbParity.FormattingEnabled = true;
+            this.cbbParity.Items.AddRange(new object[] {
+            "无",
+            "偶校验",
+            "校验",
+            "空格校验",
+            "标记校验"});
+            this.cbbParity.Location = new System.Drawing.Point(24, 158);
+            this.cbbParity.Margin = new System.Windows.Forms.Padding(4);
+            this.cbbParity.Name = "cbbParity";
+            this.cbbParity.Size = new System.Drawing.Size(129, 25);
+            this.cbbParity.TabIndex = 9;
+            // 
+            // cbbStopBits
+            // 
+            this.cbbStopBits.FormattingEnabled = true;
+            this.cbbStopBits.Items.AddRange(new object[] {
+            "无",
+            "1",
+            "2",
+            "3"});
+            this.cbbStopBits.Location = new System.Drawing.Point(24, 125);
+            this.cbbStopBits.Margin = new System.Windows.Forms.Padding(4);
+            this.cbbStopBits.Name = "cbbStopBits";
+            this.cbbStopBits.Size = new System.Drawing.Size(129, 25);
+            this.cbbStopBits.TabIndex = 8;
+            // 
+            // cbbDataBits
+            // 
+            this.cbbDataBits.FormattingEnabled = true;
+            this.cbbDataBits.Items.AddRange(new object[] {
+            "无",
+            "6",
+            "7",
+            "8"});
+            this.cbbDataBits.Location = new System.Drawing.Point(24, 91);
+            this.cbbDataBits.Margin = new System.Windows.Forms.Padding(4);
+            this.cbbDataBits.Name = "cbbDataBits";
+            this.cbbDataBits.Size = new System.Drawing.Size(129, 25);
+            this.cbbDataBits.TabIndex = 7;
+            // 
+            // cbbBaudRate
+            // 
+            this.cbbBaudRate.DisplayMember = "1";
+            this.cbbBaudRate.FormattingEnabled = true;
+            this.cbbBaudRate.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "43000",
+            "56000",
+            "57600",
+            "115200"});
+            this.cbbBaudRate.Location = new System.Drawing.Point(24, 55);
+            this.cbbBaudRate.Margin = new System.Windows.Forms.Padding(4);
+            this.cbbBaudRate.Name = "cbbBaudRate";
+            this.cbbBaudRate.Size = new System.Drawing.Size(129, 25);
+            this.cbbBaudRate.TabIndex = 6;
+            this.cbbBaudRate.ValueMember = "1";
+            // 
+            // cbbComList
+            // 
+            this.cbbComList.DisplayMember = "1";
+            this.cbbComList.FormattingEnabled = true;
+            this.cbbComList.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "COM11",
+            "COM12",
+            "COM13",
+            "COM14",
+            "COM15",
+            "COM16",
+            "COM17",
+            "COM18",
+            "COM19",
+            "COM20"});
+            this.cbbComList.Location = new System.Drawing.Point(24, 22);
+            this.cbbComList.Margin = new System.Windows.Forms.Padding(4);
+            this.cbbComList.Name = "cbbComList";
+            this.cbbComList.Size = new System.Drawing.Size(129, 25);
+            this.cbbComList.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 62);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "波特率";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(163, 161);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "校验位";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "端口";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 129);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "停止位";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 95);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "数据位";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.groupBox2);
+            this.panel2.Location = new System.Drawing.Point(236, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(747, 570);
+            this.panel2.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.COUNT);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(0, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(744, 61);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "标签设置";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "显示所有设备标签",
+            "显示选中设备标签"});
+            this.comboBox1.Location = new System.Drawing.Point(139, 21);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 23);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // COUNT
+            // 
+            this.COUNT.AutoSize = true;
+            this.COUNT.Location = new System.Drawing.Point(659, 26);
+            this.COUNT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.COUNT.Name = "COUNT";
+            this.COUNT.Size = new System.Drawing.Size(15, 15);
+            this.COUNT.TabIndex = 6;
+            this.COUNT.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(332, 25);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "过滤标签";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(430, 20);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(572, 26);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 15);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "标签数量";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(53, 26);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 15);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "标签显示";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(747, 498);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(982, 571);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "读卡器";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbbParity;
+        private System.Windows.Forms.ComboBox cbbStopBits;
+        private System.Windows.Forms.ComboBox cbbDataBits;
+        private System.Windows.Forms.ComboBox cbbBaudRate;
+        private System.Windows.Forms.ComboBox cbbComList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label COUNT;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
