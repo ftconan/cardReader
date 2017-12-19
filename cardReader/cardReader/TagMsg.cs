72a33c996335e6e6ae23684209c3f6361cb98e71
pinmingkenan <1508927793@qq.com>
2017-12-19 16:33:16 +0800

    添加标签信息类和标签信息计数类!

---
+++ cardReader/cardReader/TagMsg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardReader
+{
+    class TagMsg
+    {
+        public int DeviceId { get; set; }
+        public int ActiveId { get; set; }
+        public int TagId { get; set; }
+        public int DeviceRssi { get; set; }
+        public int TagRssi { get; set; }
+        public byte State { get; set; }
+        public DateTime ReciveDt { get; set; }
+    }
+}
---
+++ cardReader/cardReader/Temp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardReader
+{
+    class Temp
+    {
+        public TagMsg Tagmsg { get; set; }
+        public long Count { get; set; }
+
+        public Temp(TagMsg msg)
+        {
+            this.Tagmsg = msg;
+            Count = 1;
+        }
+    }
+}
